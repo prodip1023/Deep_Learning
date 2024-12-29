b5a7a4a9064d54605c03e7f966f0def5fe4a7eec
“prodip1023” <prodip1023@gmail.com>
2024-12-29 20:38:52 +0530

    add 2 project classification and regression

--- Project_Churn_Prediction/app_regression.py
+++ Project_Churn_Prediction/app_regression.py
@@ -5,34 +5,28 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
+
 # Load the trained model
 try:
-    model = tf.keras.models.load_model('model.h5')
+    model = tf.keras.models.load_model('regression_model.h5')
 except Exception as e:
     st.error(f"Error loading the model: {e}")
     st.stop()
 ## load the encoder and scaler
 try:
-    with open('onehot_encoder_geo.pkl','rb') as file:
+    with open('onehot_encoder_geo_reg.pkl','rb') as file:
         onehot_encoder_geo=pickle.load(file)
 
-    with open('label_encoder_gender.pkl', 'rb') as file:
+    with open('label_encoder_gender_reg.pkl', 'rb') as file:
         label_encoder_gender = pickle.load(file)
 
-    with open('scaler.pkl', 'rb') as file:
+    with open('scaler_reg.pkl', 'rb') as file:
         scaler = pickle.load(file)
 except Exception as e:
     st.error(f"Error loading the encoders and scalers: {e}")
     st.stop()
 
-
-## streamlit app
-# st.set_page_config("Churn Application")
-# st.header('Churn Prediction App')
-st.markdown('This app predicts the likelihood of a customer churning using various customer data.')
-st.title('Customer Churn Prediction')
-
-
+st.title('Customer Salary Prediction')
 
 
 # User Input fields 
@@ -41,11 +35,13 @@ gender = st.selectbox('👤 Gender', label_encoder_gender.classes_)
 age = st.slider('🎂 Age', 18, 92, value=30)  # Default age set to 30
 balance = st.number_input('💰 Balance', min_value=0.0, value=0.0, step=100.0)
 credit_score = st.number_input('📊 Credit Score', min_value=300, max_value=850, value=650)
-estimated_salary = st.number_input('💵 Estimated Salary', min_value=0.0, value=50000.0, step=1000.0)
+# estimated_salary = st.number_input('💵 Estimated Salary', min_value=0.0, value=50000.0, step=1000.0)
+exited = st.selectbox('💳 Has exited',[0,1])
 tenure = st.slider('📅 Tenure (Years)', 0, 10, value=5)
 num_of_products = st.slider('📦 Number of Products', 1, 4, value=5)
 has_cr_card = st.selectbox('💳 Has Credit Card', [0, 1])
 is_active_member = st.selectbox('✅ Is Active Member', [0, 1])
+
 
 # Prepare the input data
 input_data = pd.DataFrame({
@@ -57,12 +53,8 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
-
-
-
-
 # One-hot encode 'Geography'
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
@@ -78,20 +70,9 @@ except Exception as e:
     st.stop()
 
 
-
-# Predict churn
+# Predict the Salary
 try:
-    prediction = model.predict(input_data_scaled)
-    prediction_proba = prediction[0][0]
+    predicted_salary = model.predict(input_data_scaled)[0][0]
+    st.success(f"💵 Predicted Salary: {predicted_salary:,.2f}")
 except Exception as e:
-    st.error(f"Error making prediction: {e}")
-    st.stop()
-
-
-st.write("## Results")
-st.metric("Churn Probability",f"{prediction_proba:.2%}")
-
-if prediction_proba > 0.5:
-    st.error('The customer is likely to churn.')
-else:
-    st.success('The customer is not likely to churn.')
+    st.error(f"Error during prediction: {e}")
